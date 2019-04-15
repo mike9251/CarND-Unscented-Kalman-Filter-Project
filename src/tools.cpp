15e998a8031d84f1f4ee02a2c40986a077fc986b
Mikhail <soapbox92@gmail.com>
2019-04-16 00:37:43 +0300

    Add files via upload

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,4 +12,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+    
+    VectorXd rmse(4);//ground_truth[0].size());
+    rmse << 0, 0, 0, 0;
+
+    if(estimations.size() != ground_truth.size() || estimations.size() == 0){
+    	return rmse;
+    }
+
+    for(int i = 0; i < ground_truth.size(); i++){
+    	VectorXd diff = estimations[i] - ground_truth[i];
+
+    	diff = diff.array() * diff.array();
+
+    	rmse += diff;// / ground_truth.size();
+    }
+
+    rmse = rmse / ground_truth.size();
+
+    return rmse.array().sqrt();
 }
